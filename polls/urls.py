f50b380d7c3775b393a6b018ba5b8e7384021931
pabiel <panpabiel@gmail.com>
2022-11-03 22:21:02 +0100

    "lab5 - added validators and created endpoints for Person & Team model + couple of smaller changes"

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('persons/', views.person_list),
+    path('persons/<int:pk>/', views.person_detail),
+    path('persons/search/', views.get_by_names),
+    path('teams/', views.team_list),
+    path('teams/<int:pk>/', views.team_detail),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
